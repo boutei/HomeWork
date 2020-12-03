da951b10d295d1befae5dbf322ab1d5de61e15cb
boutei <boutei@users.noreply.github.com>
2020-12-04 01:12:03 +0500

    1

---
+++ HW2/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Геннадий Приказчиков
+
+//1. Написать метод, возвращающий минимальное из трех чисел.
+
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int Numbers()
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            if (a >= b)
+            {
+                a = b;
+            }
+
+            if (a >= c)
+            {
+                a = c;
+            }
+
+            return a;
+        }
+
+        static void Main()
+        {
+            int min;
+
+            Console.WriteLine("Введите 3 числа");
+
+            min = Numbers();
+
+            Console.WriteLine($"Нaименьшее число {min}");
+
+            Console.ReadKey();
+        }
+    }
+}
